6a422a34656efde578c0cb8ba0ff92bf5885851a
bokadepranav <89243706+bokadepranav@users.noreply.github.com>
2025-05-06 08:04:23 +0530

    added categories to navbar on routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import News from './Components/News';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const apiKey = "a5ad92a3c3cc4a35860bce9730bed3dd";
-  const categories = ["Business", "Entertainment", "Health", "Science", "Sports", "Technology",];
+  const categories = [
+    "general",
+    "business",
+    "entertainment",
+    "health",
+    "science",
+    "sports",
+    "technology",
+  ];
   return (
     <>
-    <Navbar categories={categories} />
-    <News pageSize={10} apiKey={apiKey} category="technology" />
+      <Router>
+        <Navbar categories={categories} />
+        <Routes>
+          {categories.map((category) => {
+            return (
+              <Route
+                path={category === "general" ? "/" : "/" + category}
+                element={
+                  <News
+                    key={category}
+                    pageSize={10}
+                    apiKey={apiKey}
+                    category={category}
+                  />
+                }
+              />
+            );
+          })}
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class Navbar extends Component {
+  capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
   render() {
     return (
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/">
             NewsMonk
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -21,17 +25,17 @@ export class Navbar extends Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
               {this.props.categories.map((category) => {
                 return (
                   <li className="nav-item" key={category}>
-                    <a className="nav-link" href="/">
-                      {category}
-                    </a>
+                    <Link
+                      className="nav-link"
+                      to={category === "general" ? "/" : "/" + category}
+                    >
+                      {category === "general"
+                        ? "Home"
+                        : this.capitalize(category)}
+                    </Link>
                   </li>
                 );
               })}
